9fb737cb51a7af973ac1ee59432ea0668cd4423d
rgonzalezs10 <rodrigojulio2005@gmail.com>
2026-03-10 18:35:09 +0000

    commit

--- esqueleto/src/contador.c
+++ esqueleto/src/contador.c
@@ -4,8 +4,35 @@ void contar(char *line, int line_number);
 
 int main(int argc, char *argv[])
 {
-  //TODO
+  //TODO comprobar que los parametros que llegan son correcto y llamar a la funcion contar
 
   return EXIT_SUCCESS;
 }
 
+void contar(char *linea, int numero_linea)
+{
+  int n_palabras = 0, dentro_palabra = 0;
+  const char *it = linea;
+
+  do
+  {
+    switch (*it)
+    {
+    case '\0':
+    case ' ':
+    case '\t':
+    case '\n':
+    case '\r':
+      if (dentro_palabra)
+      {
+        dentro_palabra = 0;
+        n_palabras++;
+      }
+      break;
+    default:
+      dentro_palabra = 1;
+    }
+  } while (*it++);
+
+  printf("[CONTADOR %d] La linea '%d' tiene %d palabras\n", getpid(), numero_linea, n_palabras);
+}
--- esqueleto/src/procesador.c
+++ esqueleto/src/procesador.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-  //TODO
+  
 
   return EXIT_SUCCESS;
 }
