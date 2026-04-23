1196ab2fb565fbd2a82b8b0aa8ec61e0d5846edd
RD1012 <ronalddenis2005@gmail.com>
2026-04-23 19:12:16 +0200

    Actualizado linea.c

--- esqueleto_3/src/linea.c
+++ esqueleto_3/src/linea.c
@@ -1,36 +1,72 @@
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-
+#include <fcntl.h>
 #include <mqueue.h>
 #include <definitions.h>
 
-// Modulo principal
-int main(int argc,char *argv[]){
+int main(int argc, char *argv[]) {
 
-    // Define variables locales
     int pid = getpid();
     mqd_t qHandlerLlamadas;
     mqd_t qHandlerLinea;
     char buzonLinea[TAMANO_MENSAJES];
-    char buffer[TAMANO_MENSAJES+1];
+    char buffer[TAMANO_MENSAJES + 1];
 
-    // Inicia Random
     srand(pid);
 
-    // Verifica los parámetros
-    if (argc != 2)
-    {
-        fprintf(stderr, "Error. Usa: ./exec/linea <cola_linea_llamante>.\n");
+    if (argc != 2) {
+        fprintf(stderr, "Error. Usa: ./exec/linea <cola_linea_llamante>\n");
         exit(EXIT_FAILURE);
     }
-    sprintf(buzonLinea,"%s",argv[1]);
 
-    //TODO
+    strcpy(buzonLinea, argv[1]);
+
+    qHandlerLlamadas = mq_open(BUZON_LLAMADAS, O_WRONLY);
+    if (qHandlerLlamadas == (mqd_t)-1) {
+        perror("Error abriendo buzon de llamadas");
+        exit(EXIT_FAILURE);
+    }
+
+    qHandlerLinea = mq_open(buzonLinea, O_RDONLY);
+    if (qHandlerLinea == (mqd_t)-1) {
+        perror("Error abriendo buzon de linea");
+        mq_close(qHandlerLlamadas);
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        printf("Linea[%d] esperando llamada...\n", pid);
+        fflush(stdout);
+
+        sleep((rand() % 30) + 1);
+
+        if (mq_send(qHandlerLlamadas, buzonLinea, strlen(buzonLinea) + 1, 0) == -1) {
+            perror("Error enviando llamada");
+            break;
+        }
+
+        printf("Linea[%d] recibida llamada (%s)...\n", pid, buzonLinea);
+        printf("Linea[%d] esperando fin de conversacion...\n", pid);
+        fflush(stdout);
+
+        memset(buffer, 0, sizeof(buffer));
+
+        if (mq_receive(qHandlerLinea, buffer, TAMANO_MENSAJES, NULL) == -1) {
+            perror("Error recibiendo fin de conversacion");
+            break;
+        }
+
+        if (strcmp(buffer, FIN_CONVERSACION) == 0) {
+            printf("Linea[%d] conversacion finalizada...\n", pid);
+            fflush(stdout);
+        }
+    }
+
+    mq_close(qHandlerLlamadas);
+    mq_close(qHandlerLinea);
 
     return EXIT_SUCCESS;
 }
-
