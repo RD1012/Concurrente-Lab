790f665c7356c40cc5769584315f356e8125b155
rgonzalezs10 <rodrigojulio2005@gmail.com>
2026-04-23 17:26:04 +0000

    Unión y añadir telefono.c

--- esqueleto_3/src/linea.c
+++ esqueleto_3/src/linea.c
@@ -24,13 +24,64 @@ int main(int argc,char *argv[]){
     // Verifica los parámetros
     if (argc != 2)
     {
-        fprintf(stderr, "Error. Usa: ./exec/linea <cola_linea_llamante>.\n");
+        fprintf(stderr, "Error. Usa: ./exec/linea <id_linea>.\n");
         exit(EXIT_FAILURE);
     }
-    sprintf(buzonLinea,"%s",argv[1]);
 
-    //TODO
+    // Construye el nombre del buzon de esta linea
+    int id_linea = atoi(argv[1]);
+    if (id_linea < 0 || id_linea >= NUMLINEAS)
+    {
+        fprintf(stderr, "Error. id_linea fuera de rango [0..%d].\n", NUMLINEAS - 1);
+        exit(EXIT_FAILURE);
+    }
+    snprintf(buzonLinea, sizeof(buzonLinea), "%s%d", BUZON_LINEAS, id_linea);
 
+    // Abre colas existentes creadas por manager
+    qHandlerLlamadas = mq_open(BUZON_LLAMADAS, O_WRONLY);
+    if (qHandlerLlamadas == (mqd_t)-1)
+    {
+        fprintf(stderr, "Linea[%d] error abriendo %s: %s\n", pid, BUZON_LLAMADAS, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    qHandlerLinea = mq_open(buzonLinea, O_RDONLY);
+    if (qHandlerLinea == (mqd_t)-1)
+    {
+        fprintf(stderr, "Linea[%d] error abriendo %s: %s\n", pid, buzonLinea, strerror(errno));
+        mq_close(qHandlerLlamadas);
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        int espera = (rand() % 30) + 1;
+        printf("Linea [%d] esperando llamada...\n", pid);
+        sleep(espera);
+
+        if (mq_send(qHandlerLlamadas, buzonLinea, strlen(buzonLinea) + 1, 0) == -1)
+        {
+            fprintf(stderr, "Linea [%d] error enviando llamada: %s\n", pid, strerror(errno));
+            continue;
+        }
+
+        printf("Linea [%d] recibida llamada (%s)...\n", pid, buzonLinea);
+        printf("Linea [%d] esperando fin de conversacion...\n", pid);
+
+        ssize_t bytes = mq_receive(qHandlerLinea, buffer, TAMANO_MENSAJES, NULL);
+        if (bytes == -1)
+        {
+            fprintf(stderr, "Linea [%d] error recibiendo fin de llamada: %s\n", pid, strerror(errno));
+            continue;
+        }
+
+        buffer[bytes] = '\0';
+        if (strcmp(buffer, FIN_CONVERSACION) == 0)
+        {
+            printf("Linea [%d] conversacion finalizada...\n", pid);
+        }
+    }
+    mq_close(qHandlerLinea);
+    mq_close(qHandlerLlamadas);
     return EXIT_SUCCESS;
 }
-
--- esqueleto_3/src/manager.c
+++ esqueleto_3/src/manager.c
@@ -55,13 +55,13 @@ int main(int argc, char *argv[])
 
 
     // Finalizamos Manager
-    printf("\n[MANAGER] Terminacion del programa (todos los procesos terminados).\n");
+    printf("\n[MANAGER] Terminacion del programa todos los procesos han terminado.\n");
     liberar_recursos();
 
     return EXIT_SUCCESS;
 }
 
-void crearbuzones() {
+void crear_buzones() {
     struct mq_attr attr;
     char nombre_buzon[64];
 
@@ -72,11 +72,19 @@ void crearbuzones() {
 
     mq_unlink(BUZON_LLAMADAS); 
     qHandlerLlamadas = mq_open(BUZON_LLAMADAS, O_CREAT | O_RDWR, 0644, &attr);
+    if (qHandlerLlamadas == (mqd_t)-1) {
+        fprintf(stderr, "[MANAGER] Error creando %s: %s\n", BUZON_LLAMADAS, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
     for (int i = 0; i < NUMLINEAS; i++) {
         sprintf(nombre_buzon, "%s%d", BUZON_LINEAS, i); 
         mq_unlink(nombre_buzon);
         qHandlerLineas[i] = mq_open(nombre_buzon, O_CREAT | O_RDWR, 0644, &attr);
+        if (qHandlerLineas[i] == (mqd_t)-1) {
+            fprintf(stderr, "[MANAGER] Error creando %s: %s\n", nombre_buzon, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
     }
 }
 
@@ -97,6 +105,10 @@ g_telefonosProcesses = n_tele;
 g_lineasProcesses = n_line;
 g_process_telefonos_table = malloc(n_tele * sizeof(struct TProcess_t));
 g_process_lineas_table = malloc(n_line * sizeof(struct TProcess_t));
+if (g_process_telefonos_table == NULL || g_process_lineas_table == NULL) {
+    fprintf(stderr, "[MANAGER] Error reservando memoria para tabla de procesos.\n");
+    exit(EXIT_FAILURE);
+}
 
 }
 
@@ -115,6 +127,10 @@ void crear_procesos(int numTele, int numLine){
 
 void lanzar_proceso_linea(int indice){
     pid_t pid = fork();
+    if (pid < 0) {
+        fprintf(stderr, "[MANAGER] Error en fork() LINEA: %s\n", strerror(errno));
+        return;
+    }
     if (pid == 0){
         char arg_id[10];
         sprintf(arg_id, "%d", indice);
@@ -122,13 +138,17 @@ void lanzar_proceso_linea(int indice){
         exit(EXIT_FAILURE);
     } else {
         g_process_lineas_table[indice].pid = pid;
-        g_process_lineas_table[indice].pid = CLASE_LINEA;
+        g_process_lineas_table[indice].clase = CLASE_LINEA;
     }
 
 }
 
 void lanzar_proceso_telefono(int indice){
     pid_t pid = fork();
+    if (pid < 0) {
+        fprintf(stderr, "[MANAGER] Error en fork() TELEFONO: %s\n", strerror(errno));
+        return;
+    }
     if (pid == 0){
         execl(RUTA_TELEFONO, CLASE_TELEFONO, NULL);
         exit(EXIT_FAILURE);
@@ -140,11 +160,34 @@ void lanzar_proceso_telefono(int indice){
 }
 
 void esperar_procesos(){
+    while (1) pause ();
+}
+
+void terminar_procesos(){
     printf("[Manager] Terminando con cualquier proceso pendiente.....\n");
     terminar_procesos_especificos(g_process_lineas_table, g_lineasProcesses);
     terminar_procesos_especificos(g_process_telefonos_table, g_telefonosProcesses);
 }
 
 void terminar_procesos_especificos(struct TProcess_t *table, int num){
-    
+for(int i = 0; i < num; i++){
+    if (table[i].pid >0){
+        printf("[Manager] Terminando proceso %s [%d]...\n", table[i].clase, table[i].pid);
+        kill(table[i].pid, SIGTERM);
+        waitpid(table[i].pid, NULL, 0);
+    }
+}
+}
+
+void liberar_recursos(){  
+char nombre[64];
+    mq_close(qHandlerLlamadas);
+    mq_unlink(BUZON_LLAMADAS);
+    for (int i = 0; i < NUMLINEAS; i++) {
+        sprintf(nombre, "%s%d", BUZON_LINEAS, i);
+        mq_close(qHandlerLineas[i]);
+        mq_unlink(nombre); 
+    }
+    free(g_process_telefonos_table); 
+    free(g_process_lineas_table);
 }
